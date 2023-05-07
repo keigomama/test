2ef4a5b00d381203b81689d0d08a79d461856950
tsuji0241@gmail.com <tsuji0241@gmail.com>
2023-05-07 16:22:48 +0900

    test OK

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("飲み物買いにいきたすぎる")
         }
         .padding()
     }
