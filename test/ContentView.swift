5955896b072ff086c27c6bbb34b38eb20bb835b1
kurano <choconogohan0504@gmail.com>
2023-05-07 17:28:10 +0900

    kurano

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("飲み物買いにいきたすぎる")
+            Text("レッサーパンダ")
         }
         .padding()
     }
