60563e41d9b8418dc0b696240dfbee7b1478092f
tsuji0241@gmail.com <tsuji0241@gmail.com>
2023-05-07 16:03:30 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by biostr1 on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
